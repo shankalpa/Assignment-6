71c2183ba3d4c5fca29f3f9988e0851745e69426
shankalpa <shan.kalp.x@gmail.com>
2016-10-10 00:40:06 -0500

    First Commit

---
+++ routes/contactus.js
@@ -0,0 +1,36 @@
+﻿var express = require('express');
+var fs = require("fs");
+var path = require("path");
+var router = express.Router();
+
+var filePath = path.join(__dirname, 'input.txt');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('contactus.html', { title: 'Contact Us'});
+});
+
+router.post('/', function (req, res) {
+    req.assert('fullName', 'full name reqiured').notEmpty();
+    req.assert('type', 'Type reqiured').notEmpty();
+    req.assert('message', 'message reqiured').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        res.json(JSON.stringify(errors));
+    } else { 
+        console.log(req.body);
+        var data = JSON.stringify(req.body);
+        fs.writeFile(filePath, data, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log('Done');
+        });
+        
+        res.json("Thank You!!")
+    }
+
+    
+});
+
+module.exports = router;
